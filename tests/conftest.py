d544a1f258e7815500d89deefb66ca21129f9a1f
Admin <alexander_kozhanov@mail.ru>
2025-05-24 11:53:31 +0300

    cocmmit

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,20 @@ import pytest
 from module.product import Product, Smartphone, LawnGrass
 from module.category import Category
 
-test_product = Product("Nokia", "ultra", 1000, 1)
-test_smasrtphone = Smartphone("Nokia", "3310", 1000, 1, 100.0, "3300", 128, "синий")
-test_lawnGrass = LawnGrass("трака", "супер травка", 100, 10, "Куба", "5 дней", "зеленая")
-test_category = Category("test", "test for hell", [test_smasrtphone])
+test_product1 = Product("Nokia", "ultra", 1000.0, 1)
+test_product2 = Product("Nokla", "Pultra", 2000.0, 1)
+test_smasrtphone = Smartphone("Nokia", "3310", 1000.0, 1, 100.0, "3300", 128, "синий")
+test_lawnGrass = LawnGrass("травка", "супер травка", 100, 10, "Куба", "5 дней", "зеленая")
+test_category = Category("Phone", "Phone it's good",[test_smasrtphone])
 
 @pytest.fixture
-def create_product():
-    return test_product
+def product_test():
+    return test_product1
 
 @pytest.fixture
-def create_category():
+def product_test_add():
+    return test_product2
+
+@pytest.fixture
+def category_test():
     return test_category
